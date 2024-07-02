340f762430506875699112e9ac64294179828561
vivekjami <j.vivekvamsi@gmail.com>
2024-07-02 15:52:32 +0530

    frontend

---
+++ src/verifedAI_icp_frontend/src/Context/SocketContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
+
+interface SocketContextType {
+  socket: Socket | null;
+}
+
+const SocketContext = createContext<SocketContextType>({ socket: null });
+
+export const useSocket = () => useContext(SocketContext);
+
+export const SocketProvider: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io("https://ai-proctor.onrender.com/"); // Replace with your server URL
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
