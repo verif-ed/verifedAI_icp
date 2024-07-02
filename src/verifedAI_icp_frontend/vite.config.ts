feeb1e1a577b2d2087abc39b4097d447e69a36a2
vivekjami <j.vivekvamsi@gmail.com>
2024-07-02 16:04:23 +0530

    frontend

--- src/verifedAI_icp_frontend/vite.config.ts
+++ src/verifedAI_icp_frontend/vite.config.ts
@@ -1,13 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { fileURLToPath, URL } from 'url';
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
 // https://vitejs.dev/config/
+dotenv.config({ path: '../../.env' });
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:4943",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [
     react(),
     nodePolyfills({
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
     }),
+    environment("all", { prefix: "CANISTER_" }),
+    environment("all", { prefix: "DFX_" }),
   ],
+  resolve: {
+    alias: [
+      {
+        find: "declarations",
+        replacement: fileURLToPath(
+          new URL("../declarations", import.meta.url)
+        ),
+      },
+    ],
+  },
 });
